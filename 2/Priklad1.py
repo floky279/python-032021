79d044a04a3da035ba06a91244167b9d5da5935b
Veronika Flokova <floky279@gmail.com>
2021-10-26 19:25:43 +0200

    Druha domaca uloha

--- 2/Priklad1.py
+++ 2/Priklad1.py
@@ -7,17 +7,11 @@ with requests.get("https://raw.githubusercontent.com/pesikj/progr2-python/master
 president_elections = pandas.read_csv("1976-2020-president.csv")
 print(president_elections.head())
 
-#Urči pořadí jednotlivých kandidátů v jednotlivých státech a v jednotlivých letech (pomocí metody rank()). Nezapomeň, že data je před použitím metody nutné seřadit a spolu s metodou rank() je nutné použít metodu groupby().
+president_elections["Rank"] = president_elections.groupby(["state", "year"])["candidatevotes"].rank(ascending=False)
 
-president_elections["Rank"] = president_elections.groupby(["year"])["candidatevotes"].rank(method="max", ascending=False)
-
-print(president_elections)
-print(president_elections.sort_values(["Rank"]).head())
+president_elections_sorted = president_elections.sort_values(["Rank", "state", "party_simplified", "year"])
+president_elections_sorted["previous_election_party"] = president_elections_sorted.groupby(["Rank"])["party_simplified"].shift()
 
 
-president_elections_sorted = president_elections.sort_values(["candidatevotes", "year"])
-president_elections_sorted["Rank Previous Year"] = president_elections_sorted.groupby(["candidatevotes"])["Rank"].shift()
-print(president_elections_sorted.head())
-
-president_elections_sorted["diff"]=numpy.where(president_elections_sorted["Rank"] == president_elections_sorted["Rank Previous Year"], 1, 0)
+president_elections_sorted["diff"] = numpy.where(president_elections_sorted["party_simplified"] == president_elections_sorted["previous_election_party"], 0, 1)
 print(president_elections_sorted.head())
