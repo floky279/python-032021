df313efba055883220a27e34a1f848c0382e6819
Veronika Flokova <floky279@gmail.com>
2021-11-02 11:42:53 +0100

    Tretia domaca uloha

--- 3/1 ukol.py
+++ 3/1 ukol.py
@@ -4,11 +4,11 @@ import requests
 r = requests.get("https://raw.githubusercontent.com/lutydlitatova/czechitas-datasets/main/datasets/lexikon-zvirat.csv")
 open("lexikon-zvirat.csv", "wb").write(r.content)
 
-lexikon_zvirat = pandas.read_csv("lexikon-zvirat.csv", sep=";")
-lexikon_zvirat = lexikon_zvirat.dropna(how="all", axis="columns")
-lexikon_zvirat = lexikon_zvirat.dropna(how="all", axis="rows")
-lexikon_zvirat = lexikon_zvirat.set_index("id")
-print(lexikon_zvirat)
+lexikon = pandas.read_csv("lexikon-zvirat.csv", sep=";")
+lexikon = lexikon.dropna(how="all", axis="columns")
+lexikon = lexikon.dropna(how="all", axis="rows")
+lexikon = lexikon.set_index("id")
+# print(lexikon)
 
 # Napiš funkci check_url, která bude mít jeden parametr radek. Funkce zkontroluje, jestli je odkaz v pořádku podle několika pravidel. K odkazu přistoupíš v těle funkce přes tečkovou notaci: radek.image_src. Zkontroluj následující:
 #
@@ -16,10 +16,16 @@ print(lexikon_zvirat)
 # hodnota začíná řetězcem "https://zoopraha.cz/images/": radek.image_src.startswith("https://zoopraha.cz/images/") 3.hodnota končí buďto JPG nebo jpg.
 # Zvol si jeden ze způsobů procházení tabulky, a na každý řádek zavolej funkci check_url. Pro každý řádek s neplatným odkazem vypiš název zvířete (title).
 
+chybne_url = []
 
-for radek in lexikon_zvirat.itertuples():
-    check_url = isinstance(radek.image_src, str)
-    check_url = radek.image_src.startswith("https://zoopraha.cz/images/")
-    check_url = radek.image_src.endswith("JPG") or radek.image_src.endswith("jpg")
 
+def check_url(radek):
+    if not isinstance(radek.image_src, str):
+        if not radek.image_src.startswith("https://zoopraha.cz/images/"):
+            if not radek.image_src.endswith("jpg"):
+             return "chyba"
+
+
+for radek in lexikon.itertuples():
+    lexikon[chybne_url] = check_url(radek)
     print(radek.title)
